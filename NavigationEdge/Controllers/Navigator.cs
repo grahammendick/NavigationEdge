3c48de4765a97af2a242ac525879db5a0e7ff60a
Graham Mendick <graham.mendick@gmail.com>
2015-08-27 14:39:53 +0100

    Added render Edge function

--- NavigationEdge/Controllers/Navigator.cs
+++ NavigationEdge/Controllers/Navigator.cs
@@ -1,6 +1,7 @@
 ﻿using EdgeJs;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -23,8 +24,30 @@ namespace NavigationEdge.Controllers
 			}
 		");
 
-		public static Task<object> GetStateContext(string url) {
+		private static Func<object, Task<object>> render = Edge.Func(@"
+			var React = require('react');
+			var Navigation = require('navigation');
+			var StateInfo = require('../../../node/StateInfo');
+				
+			StateInfo.register();
+			return function (data, callback) {
+				Navigation.StateController.navigateLink(data.url);
+				var component = React.createElement(NavigationShared.getComponent(), data.props);
+				callback(null, React.renderToString(component));
+			}
+		");
+
+		public static Task<object> GetStateContext(string url)
+		{
 			return getStateContext(url);
+		}
+
+		public static Task<object> Render(string url, dynamic props)
+		{
+			dynamic data = new ExpandoObject();
+			data.url = url;
+			data.props = props;
+			return render(data);
 		}
 	}
 }
