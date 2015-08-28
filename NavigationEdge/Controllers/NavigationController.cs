c9443d40d124f5332f005233670d76fc347dfd15
Graham Mendick <graham.mendick@gmail.com>
2015-08-28 12:41:02 +0100

    Tidied up usings

--- NavigationEdge/Controllers/NavigationController.cs
+++ NavigationEdge/Controllers/NavigationController.cs
@@ -1,10 +1,8 @@
 ﻿using NavigationEdge.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-using System.Web.Script.Serialization;
 
 namespace NavigationEdge.Controllers
 {
