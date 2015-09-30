b64b1d77177b9129847ca3486aae0f03e424f4ae
Graham Mendick <graham.mendick@gmail.com>
2015-09-30 16:01:43 +0100

    Added Person Controller

---
+++ NavigationEdgeApi/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using NavigationEdgeApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+
+namespace NavigationEdgeApi.Controllers
+{
+    public class PersonController : ApiController
+    {
+		public Dictionary<string, Person> Get([ModelBinder] int id)
+		{
+			var person = new Data().GetPerson(id);
+			return new Dictionary<string, Person>() { { "person", person } };
+		}
+	}
+}
